d6892b1478463f99829fab1958733e12e7ad0d4f
Avvessalom <49491863+Avvessalom@users.noreply.github.com>
2019-07-25 15:25:49 +0300

    implemented work with files

--- Lab_5/src/com/company/Commands.java
+++ Lab_5/src/com/company/Commands.java
@@ -1,6 +1,7 @@
-/*package com.company;
+package com.company;
 
 public class Commands {
+    //
 }
-*/
+
 
--- Lab_5/src/com/company/Main.java
+++ Lab_5/src/com/company/Main.java
@@ -5,35 +5,34 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String[] list = {"one", "two", "three", "fo"};
-        try {
-            File file = new File("C:\\Users\\Erwin Eugen Rommel\\Documents\\GitHub\\ITMO_programming_lab\\Lab_5\\Characters.txt");
-            FileWriter fileReader = new FileWriter(file); // поток который подключается к текстовому файлу
-            BufferedWriter bufferedWriter = new BufferedWriter(fileReader); // соединяем FileWriter с BufferedWitter
+    public static void main(String[] args) throws FileNotFoundException {
+        String argue = null, newCommand;
+        String Command;
+        File file = new File("C:\\Users\\Erwin Eugen Rommel\\Documents\\GitHub\\ITMO_programming_lab\\Lab_5\\Characters.txt"); // создакм экземпляр файла
+        ArrayDeque<Characters> characters = new ArrayDeque<>();
 
-            for (String s : list) {
-                bufferedWriter.write(s + "\n");
+        try{
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in); // поток чтения с консоли
+            FileWriter fileReader = new FileWriter(file); // поток который подключается к текстовому файлу
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // соединяем FileReader с BufferedReader
+            BufferedWriter bufferedWriter = new BufferedWriter(fileReader); //соединяем BufferedWriter c FileWriter
+
+            String line;
+            while(!(line = bufferedReader.readLine()).equals("exit")){
+                bufferedWriter.write(line+"\n"); // чтение и запись из командной строки с переносом
             }
 
-            bufferedWriter.close(); // закрываем поток
-        } catch (Exception e) {
+            bufferedReader.close(); // закрываем поток
+            bufferedWriter.close();
+        }catch (Exception e){
             e.printStackTrace();
         }
+    
 
 
 
 
-        ArrayDeque<Characters> characters = new ArrayDeque<>();
-        Scanner scanner = new Scanner(file);
-        Scanner sc = new Scanner(System.in);
 
-            SortedSet<String> countrySet = new TreeSet<>();
-            countrySet.add("Россия");
-            countrySet.add("Франция");
-            countrySet.add("Гондурас");
-            countrySet.add("Кот-Д'Ивуар"); // любимая страна всех котов
-
-            mInfoTextView.setText(countrySet.toString());
     }
 }
